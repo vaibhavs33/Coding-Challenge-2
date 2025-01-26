417373270a92045f6a66fdf88b4fae52e5bad09f
Vaibhav Surapaneni <vaibhavsurapaneni1979@gmail.com>
2025-01-26 10:26:16 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,13 @@
+//Task 1 - Store Inventory
+
+//Declare an array called products with initial product names
+let products = ["Desk", "Chair", "Lamp", "Bookshelf", "Couch"];
+
+//Add a new product to the array
+products.push("Table");
+
+//Remove the last product from the array
+products.pop();
+
+//Logging the updated product list to the console
+console.log("Updated product list:", products);
