8bdcac52588782f408a3ba36338570c0de9c9a10
Vaibhav Surapaneni <vaibhavsurapaneni1979@gmail.com>
2025-01-26 10:38:08 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -11,3 +11,19 @@ products.pop();
 
 //Logging the updated product list to the console
 console.log("Updated product list:", products);
+
+//Task 2 - Student Scores
+
+//Declare an array called scores with initial numerical values
+let scores = [83, 92, 80, 86, 95];
+
+//Updating the second score in the array
+scores[1] = 94;
+
+//Calculating the average of all scores
+let total = scores.reduce((sum, score) => sum + score, 0);
+let average = total / scores.length;
+
+//Logging the updated array and the average score to the console
+console.log("Updated scores:", scores);
+console.log("Average score:", average);
